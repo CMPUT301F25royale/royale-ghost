9de6795b14df8f0b705329ee17888de0624acd13
aidanslator <aslator@ualberta.ca>
2025-10-31 15:56:53 -0600

    Organizer navigation in progress

--- code/app/src/main/java/com/example/project_part_3/Users/Organizer/Organizer_main_fragment.java
+++ code/app/src/main/java/com/example/project_part_3/Users/Organizer/Organizer_main_fragment.java
@@ -2,5 +2,5 @@ package com.example.project_part_3.Users.Organizer;
 
 import androidx.fragment.app.Fragment;
 
-public class Organizer_main extends Fragment {
+public class Organizer_main_fragment extends Fragment {
 }
