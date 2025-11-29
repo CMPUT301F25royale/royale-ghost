dedb83f3d4fb9a41fd5bd2caaab777d20278cdf6
aidanslator <aslator@ualberta.ca>
2025-11-28 18:01:27 -0700

    Update Database.java

--- code/app/src/main/java/com/example/project_part_3/Database_functions/Database.java
+++ code/app/src/main/java/com/example/project_part_3/Database_functions/Database.java
@@ -1,5 +1,6 @@
 package com.example.project_part_3.Database_functions;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -78,6 +79,13 @@ public class Database {
     }
 
 
+    /**
+     * Add a user to the database.
+     *
+     * @param user The user to add to the database.
+     * @return A task that completes when the user is added to the database.
+     */
+
     public Task<Boolean> addUser(@NonNull User user) {
         return doesUserExist(user).continueWithTask(task -> {
             boolean exists = task.getResult();
@@ -91,6 +99,14 @@ public class Database {
         });
     }
 
+    /**
+     * Check if a user exists in the database.
+     *
+     * @param email The email of the user to check.
+     * @param password The password of the user to check.
+     * @return A task that completes when the user is checked.
+     */
+
     public Task<User> checkUser(String email, String password) {
         return fetchUser(email).continueWith(task -> {
             if (!task.isSuccessful()) {
@@ -100,7 +116,7 @@ public class Database {
             if (user == null) {
                 throw new Exception("User not found");
             }
-            if (android.text.TextUtils.equals(user.getPassword(), password)) {
+            if (TextUtils.equals(user.getPassword(), password)) {
                 return user;
             } else {
                 throw new Exception("Incorrect password");
@@ -108,6 +124,12 @@ public class Database {
         });
     }
 
+    /**
+     * Fetch a user from the database.
+     *
+     * @param email The email of the user to fetch.
+     * @return A task that completes when the user is fetched.
+    */
     public Task<User> fetchUser(String email) {
         DocumentReference docRef = db.collection(USERS_COLLECTION).document(email);
 
@@ -138,10 +160,20 @@ public class Database {
         });
     }
 
+    /**
+     * Set a user in the database.
+     *
+     * @param user The user to set in the database.
+     */
+
     public void setUser(User user) {
         db.collection(USERS_COLLECTION).document(user.getEmail()).set(user);
     }
 
+    /** Get a list of all users from the database.
+     *
+     * @return A task that contains a list of all users in the database.
+     */
     public Task<List<User>> getAllUsers() {
 
         return db.collection(USERS_COLLECTION).get().continueWith(task -> {
@@ -154,6 +186,11 @@ public class Database {
         });
     }
 
+    /** Check if a user exists in the database.
+     *
+     * @param user The user to check.
+     * @return A task that completes when the user is checked.
+     */
     public Task<Boolean> doesUserExist(User user) {
 
         return fetchUser(user.getEmail()).continueWith(task -> {
@@ -165,6 +202,12 @@ public class Database {
         });
     }
 
+    /**
+     * Add an event to the database.
+     *
+     * @param event The event to add to the database.
+     * @return A task that completes when the event is added to the database.
+     */
     public Task<Boolean> addEvent(@NonNull Event event) {
 
         DocumentReference docRef = db.collection(USERS_COLLECTION).document(event.getOrganizerId())
@@ -173,6 +216,11 @@ public class Database {
         return docRef.set(event).continueWith(setTask -> setTask.isSuccessful());
     }
 
+    /** Fetch an event from the database.
+     *
+     * @param eventId The ID of the event to fetch.
+     * @return A task that completes when the event is fetched.
+     */
     public Task<Event> fetchEvent(String eventId) {
 
         return db.collectionGroup(EVENTS_SUBCOLLECTION).whereEqualTo("id", eventId).limit(1).get()
@@ -196,6 +244,12 @@ public class Database {
         });
     }
 
+    /**
+     * Get a list of all events organized by an organizer.
+     *
+     * @param email The email of the organizer.
+     * @return A task that contains a list of all events hosted by the organizer.
+     */
     public Task<List<Event>> getEventsByOrganizer(@NonNull String email) {
 
         return db.collection(USERS_COLLECTION).document(email).collection(EVENTS_SUBCOLLECTION).get()
@@ -208,6 +262,12 @@ public class Database {
                 });
     }
 
+    /**
+     * Delete a user from the database.
+     *
+     * @param email The email of the user to delete.
+     * @return A task that completes when the user is deleted.
+     */
     public Task<Boolean> deleteUser(String email) {
         DocumentReference userDocRef = db.collection(USERS_COLLECTION).document(email);
         return db.runTransaction(transaction -> {
@@ -234,6 +294,11 @@ public class Database {
                 .addOnFailureListener(e -> Log.e("deleteUser", "Failed to delete user", e));
     }
 
+    /** Delete an event from the database.
+     *
+     * @param event The event to delete.
+     * @return A task that completes when the event is deleted.
+     */
     public Task<Boolean> deleteEvent(Event event) {
 
         DocumentReference docRef = db.collection(USERS_COLLECTION).document(event.getOrganizerId())
@@ -241,6 +306,12 @@ public class Database {
         return docRef.delete().continueWith(Task::isSuccessful);
     }
 
+    /**
+     * Update an event in the database.
+     *
+     * @param event The event to update.
+     * @return A task that completes when the event is updated.
+     */
     public Task<Boolean> updateEvent(Event event) {
         DocumentReference docRef = db.collection(USERS_COLLECTION).document(event.getOrganizerId());
         docRef = docRef.collection(EVENTS_SUBCOLLECTION).document(event.getId());
@@ -262,21 +333,41 @@ public class Database {
                 });
     }
 
-    /** Add a user to the waitlistUserIds array for an event located via collectionGroup(id). */
+    /**
+     * Add a user to the waitlistUserIds array for an event located via collectionGroup(id).
+     *
+     * @param eventId The ID of the event to add the user to.
+     * @param userId The ID of the user to add to the waitlist.
+     * @return A task that completes when the user is added to the waitlist.
+     */
     public Task<Void> addUserToWaitlistById(@NonNull String eventId, @NonNull String userId) {
         return findEventDocById(eventId)
                 .continueWithTask(t -> t.getResult()
-                        .update("waitlistUserIds", com.google.firebase.firestore.FieldValue.arrayUnion(userId)));
+                        .update("waitlistUserIds", FieldValue.arrayUnion(userId)));
     }
 
-    /** Remove a user from the waitlistUserIds array for an event located via collectionGroup(id). */
+    /**
+     *
+     * Remove a user from the waitlistUserIds array for an event located via collectionGroup(id).
+     *
+     * @param eventId The ID of the event to remove the user from.
+     * @param userId The ID of the user to remove from the waitlist.
+     * @return A task that completes when the user is removed from the waitlist.
+     */
     public Task<Void> removeUserFromWaitlistById(@NonNull String eventId, @NonNull String userId) {
         return findEventDocById(eventId)
                 .continueWithTask(t -> t.getResult()
-                        .update("waitlistUserIds", com.google.firebase.firestore.FieldValue.arrayRemove(userId)));
+                        .update("waitlistUserIds", FieldValue.arrayRemove(userId)));
     }
 
-    /** Check whether a given user is on the waitlist for an event found via collectionGroup(id). */
+    /**
+     * Check whether a given user is on the waitlist for an event found via collectionGroup(id).
+     *
+     * @param eventId The ID of the event to check.
+     * @param userId The ID of the user to check.
+     *
+     * @return A task that completes when the user is checked.
+     */
     public Task<Boolean> isUserOnWaitlistById(@NonNull String eventId, @NonNull String userId) {
         return findEventDocById(eventId)
                 .continueWithTask(t -> t.getResult().get())
@@ -288,6 +379,13 @@ public class Database {
                 });
     }
 
+    /**
+     * Get a list of all events a user is on the waitlist for.
+     *
+
+     * @param userId The ID of the user to check.
+     * @return A task that contains a list of events that the specified user has waitlisted.
+     */
     public Task<List<Event>> getEventsUserWaitlisted(@NonNull String userId) {
         return db.collectionGroup(EVENTS_SUBCOLLECTION)
                 .whereArrayContains("waitlistUserIds", userId)
@@ -314,12 +412,25 @@ public class Database {
                 });
     }
 
-    // Waitlist functions
+    /**
+     * Add a user to the waitlist for an event.
+     *
+     * @param eventId The ID of the event which contains the waitlist to add the user to.
+     * @param userEmail The email of the user to add to the waitlist.
+     * @return A task that completes when the user is added to the waitlist.
+     */
     public Task<Void> addUserToWaitlist(@NonNull String eventId, @NonNull String userEmail) {
         return findEventDocRefById(eventId)
                 .onSuccessTask(ref -> ref.update("waitlistUserIds", FieldValue.arrayUnion(userEmail)));
     }
 
+    /**
+     * Remove a user from the waitlist for an event.
+     *
+     * @param eventId The ID of the event which contains the waitlist to remove the user from.
+     * @param userEmail The email of the user to remove from the waitlist.
+     * @return A task that completes when the user is removed from the waitlist.
+     */
     // TODO: Wire this to fire when user clicks cancel button on the "events youve signed up for" tab
     public Task<Void> removeUserFromWaitlist(@NonNull String eventId, @NonNull String userEmail) {
         removeEventFromUser(userEmail, eventId);
@@ -327,6 +438,13 @@ public class Database {
                 .onSuccessTask(ref -> ref.update("waitlistUserIds", FieldValue.arrayRemove(userEmail)));
     }
 
+    /**
+     * Check whether a user is on the waitlist for an event.
+     *
+     * @param eventId The ID of the event to check.
+     * @param userEmail The email of the user to check.
+     * @return A task that completes when the user is checked.
+     */
     public Task<Boolean> isUserOnWaitlist(@NonNull String eventId, @NonNull String userEmail) {
         return findEventDocRefById(eventId)
                 .onSuccessTask(ref -> ref.get())
@@ -337,6 +455,12 @@ public class Database {
                 });
     }
 
+    /**
+     * Get a list of all events a user is on the waitlist for.
+     *
+     * @param userId The ID of the user to check.
+     * @return A task that contains a list of events that the specified user has waitlisted.
+     */
     public Task<List<String>> getEventsUserSelected(@NonNull String userId) {
         return db.collection(USERS_COLLECTION).whereEqualTo("email", userId)
                 .limit(1)
